daf2d9f2780966e1ecd50d9f0148578a3fd2e302
Maisa Sandrini de castro <maisasandrini@Maisas-MacBook-Air.local>
2018-02-21 17:01:34 +1100

    Building the application using concepts of containers and reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+
+	return [
+			{ title: 'Harry Potter', pages: 130},
+			{ title: 'JavaScript', pages: 900 },
+			{ title: 'The Witch of Portobello' , pages: 125},
+			{ title: 'The Alchemist', pages: 98 }
+	]
+}
